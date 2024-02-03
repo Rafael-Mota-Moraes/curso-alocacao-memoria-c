10f6a5771686a9fdffc5a43fdb679b113647d95c
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-02-03 07:33:43 -0300

    Introdução aos ponteiros e à memória principal

---
+++ ponteiros/introducao.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int x; // Alocado na área de dados estáticos da memória RAM.
+
+int main(void)
+{
+    int x = 120;
+    printf("Vamos aprender sobre ponteiros!!!\n");
+    return 0;
+}
